0d9d4bd8a44fbc6fb094674a16762cfa3d959a57
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2021-02-02 07:28:08 -0500

    Packaging a cli tool

---
+++ bin/win-info.js
@@ -0,0 +1,8 @@
+const { getActiveSync, getByPidSync } = require('..');
+const args = process.argv.slice(2);
+
+if (args.length) {
+  console.log(getByPidSync(args[0]));
+} else {
+  console.log(getActiveSync());
+}
